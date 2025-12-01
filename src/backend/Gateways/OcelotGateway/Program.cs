03f1784329ceb8b5571b5646f856f42dc7219dc1
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-01 11:12:45 +0700

    feat(api-gateway): Configure JWT authentication and secure Basket route

--- src/backend/Gateways/OcelotGateway/Program.cs
+++ src/backend/Gateways/OcelotGateway/Program.cs
@@ -1,18 +1,39 @@
-﻿using Ocelot.DependencyInjection;
+﻿using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Add Configuration (Đọc file ocelot.json)
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// 2. Add Ocelot Service
+// 1. Thêm Authentication Service
+var secretKey = builder.Configuration["JwtSettings:Secret"];
+var key = Encoding.UTF8.GetBytes(secretKey!);
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// 3. Use Ocelot Middleware
-// Phải dùng await vì Ocelot chạy bất đồng bộ
+// 2. Kích hoạt Authentication Middleware (Trước UseOcelot)
+app.UseAuthentication();
+app.UseAuthorization(); // (Optional nhưng nên có)
+
 await app.UseOcelot();
 
 app.Run();
