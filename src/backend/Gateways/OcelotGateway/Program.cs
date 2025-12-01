7f9dc8e677408c4018f4820e628320291631818d
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-12-01 10:03:36 +0700

    feat(api-gateway): Implement Ocelot Gateway and configure routing

--- src/backend/Gateways/OcelotGateway/Program.cs
+++ src/backend/Gateways/OcelotGateway/Program.cs
@@ -1,6 +1,18 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Add Configuration (Đọc file ocelot.json)
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// 2. Add Ocelot Service
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 3. Use Ocelot Middleware
+// Phải dùng await vì Ocelot chạy bất đồng bộ
+await app.UseOcelot();
 
 app.Run();
